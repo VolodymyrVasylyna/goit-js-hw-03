ce1be3044cdfcb83ef8ae725eac875d5613bc1c7
VolodymyrVasylyna <143510572+VolodymyrVasylyna@users.noreply.github.com>
2023-12-20 12:27:56 +0200

    done

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+
+  const slug = title.toLowerCase().replace(/[^a-z0-9]/g, '-');
+
+  return slug.replace(/^-+|-+$/g, '');
+}
+
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
